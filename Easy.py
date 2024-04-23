448aaa7f4aacb87341cbda958bf3ebd601e2d7c6
alesziegler <alesziegler@gmail.com>
2024-04-23 23:30:46 +0200

    Update Easy.py

--- Easy.py
+++ Easy.py
@@ -4,7 +4,14 @@ import datetime
 print("Vypisuji součty cifer v datu pro následujících 7 dní:")
 
 counter = 0
+manipulated_date = datetime.datetime.now().date()
+
 
 while counter <= 7:
-    print(str(datetime.datetime.today))
+    try:
+        print(manipulated_date)
+        manipulated_date = manipulated_date.replace(day=manipulated_date.day+1)
+    except:
+
+        print("overflow")
     counter += 1
