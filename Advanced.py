9fa289be0b13fa096b378a5ab13326f9100fa7d5
alesziegler <alesziegler@gmail.com>
2024-04-24 23:29:38 +0200

    final

--- Advanced.py
+++ Advanced.py
@@ -3,16 +3,17 @@ import datetime
 
 print("Pro zadaný interval datumů vypíše všechny pátky 13.:")
 
-print(datetime.datetime(2023, 3, 30))
-
 first_date = datetime.datetime.strptime(input("Zadejte 1. datum (př. 1.1.2000):\n"),"%d.%m.%Y")
 
 second_date = datetime.datetime.strptime(input("Zadejte 2. datum:\n"),"%d.%m.%Y")
 
 anchor_date = first_date
 
+fridays_13th = ""
+
 while anchor_date <= second_date:
-    print("test")
+    #print("test")
     if anchor_date.day == 13 and anchor_date.isoweekday() == 5:
-        print("Friday 13th")
-    anchor_date = anchor_date.replace(day=anchor_date.day+1)
+        fridays_13th += f"{str(anchor_date.date())}, "
+    anchor_date += datetime.timedelta(days=1)
+print(f"Patky 13: {fridays_13th}")
