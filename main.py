40f5976103c2bd217a5e27ea4b6dc04bef4ac072
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-04-26 12:38:18 +0000

    Constructing birthday method

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("test")
-print("again")
+from osoba import Osoba
+
+#from datetime import datetime
+
+#random_date = datetime(datetime.today().year,5,12)
+
+#print(random_date.date())
+
+print("Zadejte datum narození: (př.: 1.1.2000)")
+datumText = input()
+
+someone = Osoba(datumText)
+
+print(someone.birthday())
+
+print(f"Je ti x let a narozeniny máš za y dní.")
--- osoba.py
+++ osoba.py
@@ -1,1 +1,20 @@
+#import datetime
+
+from datetime import datetime, timedelta
+
 class Osoba:
+
+  
+
+  def __init__(self,birth_date):
+    self.birth_date = datetime.strptime(birth_date, "%d.%m.%Y")
+    
+
+  def birthday(self):
+    birthday = datetime(datetime.today().year,self.birth_date.month,self.birth_date.day).date()
+    return str(birthday)
+
+  def how_old_they_are(self):
+    #if birthdate_day+month lower than today, something
+    #else something +/- 1
+    pass
